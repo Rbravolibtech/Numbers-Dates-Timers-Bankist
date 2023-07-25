8c5f04355f0ba62b9bc8bd01e16a11cb8752e804
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-25 14:34:47 -0600

    173 numeric separators js

--- script.js
+++ script.js
@@ -352,3 +352,23 @@ labelBalance.addEventListener('click', function () {
     if (i % 3 === 0) row.style.backgroundColor = 'blue';
   });
 });
+
+//////////// NUMERIC SEPARATORS ////////////////
+
+// 287,460,000,000 ////javascript ignores the under scores
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+//$345.99
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230_000'));
+console.log(parseInt('230_000'));
